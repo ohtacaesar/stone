69a9ad49a4307335278349c309c890fb5c777d9f
Takahiro Otomo <ohtacaesar@users.noreply.github.com>
2012-10-12 21:56:26 +0900

    fixed Regular expression

--- lib/stone/lexer.rb
+++ lib/stone/lexer.rb
@@ -1,24 +1,23 @@
 # -*- coding: utf-8 -*-
+require 'strscan'
+
 module Stone
   class Lexer
 
-    def initialize(reader)
+    def initialize(reader = nil)
       @queue    = Array.new
       @has_more = true
       @reader   = reader
 
-      @comment_pattern = /\/\/.*/
-      @num_pattern     = /\d+/
-      @string_pattern  = /"(\"|\\|\n|[^"])*\"/
-      @id_pattern      = /[A-Z_a-z][A-Z_a-z0-9]*|\+|-|\*|\/|==|<=|>=|&&|\|\||\p{Punct}/
-      # @pattern         = Regexp.union(
-      #                              @comment_pattern,
-      #                              @num_pattern,
-      #                              @string_pattern,
-      #                              @id_pattern
-      #                              )
-      @pattern = /(\/\/)|([\d]+)|("(\"|\\|\n|[^\"])*\")|([A-Z_a-z][A-Z_a-z0-9]*|\+|\*\/|==|<=|<|>=|>)/
-      puts @pattern
+      @patterns = Hash.new
+      @patterns[:comment] = /(\s*)(\/\/.*)/
+      @patterns[:number]  = /(\s*)([0-9]+)/
+      @patterns[:string]  = /(\s*)("(\"|\\|\n|[^"])*\")/
+      @patterns[:id]      = /(\s*)([A-Z_a-z][A-Z_a-z0-9]*|==|<=|>=|&&|\|\||=|\+|-|\*|\/)/
+    end
+
+    def set_reader(reader)
+      @reader = reader
     end
 
     def read
@@ -54,46 +53,36 @@ module Stone
         @has_more = false
         return
       end
-      line.scan(@pattern).each do |item|
-        self.add_token(@reader.line_number, item)
-      end
-      @queue.push(IdToken.new(@reader.line_number, Token.EOL))
-    end
+      line.chomp!
+      # puts "\n\nline = '#{line}'"
 
-    def add_token(line_number, match_data)
-      unless match_data[0] #comment
-        if match_data[1] #number
-          token = NumToken.new(line_number, match_data[1].to_i)
-          @queue.push(token)
-        elsif match_data[2] #string
-          token = StrToken.new(line_number, self.to_string_literal(match_data[2]))
-          @queue.push(token)
-        elsif match_data[3] # identifier
-          token = IdToken.new(line_number, match_data[3])
-          @queue.push(token)
-        end
-      end
-    end
-
-    def to_string_literal(string)
-      result = ""
-      len = string.size + 1
-      i = 0
-      while i < len
-        c = string[i]
-        if c == '\\' && i + 1 < len
-          c2 = string[i + 1]
-          if c2 == '"' || c2 == '\\'
-            i += 1
-            c = string[i]
-          elsif c2 == 'n'
-            i += 1
-            c = '\n';
+      string_scanner = StringScanner.new(line)
+      flag = false
+      while string_scanner.rest?
+        # マッチしないと無限ループに
+        @patterns.each do |key, value|
+          if item = string_scanner.scan(value)
+            item.strip!
+            # puts "line_number => #{@reader.line_number.to_s}, item => #{item}, key => #{key.to_s}"
+            self.add_token(@reader.line_number, item, key)
+            break
           end
         end
-        result += c
       end
-      result
+      @queue << IdToken.new(@reader.line_number, Token.EOL)
+    end
+
+    def add_token(line_number, match_data, key)
+      token = nil
+      case key
+      when :number
+        token = NumToken.new(line_number, match_data.to_i)
+      when :string
+        token = StrToken.new(line_number, match_data)
+      when :id
+        token = IdToken.new(line_number, match_data)
+      end
+      @queue << token unless token == nil
     end
   end
 end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+require 'minitest/unit'
+require 'minitest/autorun'
+
+$LOAD_PATH << File.dirname(__FILE__) + '/../lib'
+require 'stone'
