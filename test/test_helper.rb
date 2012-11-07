414981b8d51536cb18a67a459a82d647f4fb7a0a
Takahiro Ohtomo <ohtacaesar@Ohtacaesars-MacBook-Air.local>
2012-11-08 02:21:01 +0900

    fix expr_parser and remove unnecessary files

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
 require 'minitest/unit'
 require 'minitest/autorun'
-
-$LOAD_PATH << File.dirname(__FILE__) + '/../lib'
-require 'stone'
+require File.dirname(__FILE__) + '/../lib/stone'
