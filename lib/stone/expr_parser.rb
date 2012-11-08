51958a719cde89d5e8fc3406e1b72cdd170721e5
Takahiro Ohtomo <ohtacaesar@net75-dhcp168.sfc.keio.ac.jp>
2012-11-08 14:50:53 +0900

    こいつ動くぞ！

--- lib/stone/expr_parser.rb
+++ lib/stone/expr_parser.rb
@@ -2,53 +2,155 @@
 
 module Stone
   class ExprParser
-    attr :lexer
+    class Precedence
+      attr :value, :left_assoc
+      def initialize(v, a)
+        @value = v
+        @left_assoc = a
+      end
+    end
+
+    attr :lexer, :operators
 
     def initialize(lexer)
       @lexer = lexer
+      @operators = {}
+      @operators["<"]  = Precedence.new(1, true)
+      @operators["<="] = Precedence.new(1, true)
+      @operators[">"]  = Precedence.new(1, true)
+      @operators[">="] = Precedence.new(1, true)
+      @operators["=="] = Precedence.new(1, true)
+      @operators["!="] = Precedence.new(1, true)
+      @operators["+"]  = Precedence.new(2, true)
+      @operators["-"]  = Precedence.new(2, true)
+      @operators["*"]  = Precedence.new(3, true)
+      @operators["/"]  = Precedence.new(3, true)
+      @operators["^"]  = Precedence.new(4, false)
+      @operators["="]  = Precedence.new(4, false)
     end
 
-    def expression
-      tree_left = term
-      while is_token?("+") || is_token?("-")
-        op = Ast::AstLeaf.new(@lexer.read)
-        tree_right = term
-        tree_left = Ast::BinaryExpr.new([tree_left, op, tree_right])
+    def do_shift(left, prec)
+      op = Ast::AstLeaf.new(@lexer.read)
+      right = factor
+      while (n = next_operator) != nil && right_is_expr(prec, n)
+        right = do_shift(right, n.value)
       end
-      tree_left
+
+      Ast::BinaryExpr.new([left, op, right])
     end
 
-    def term
-      tree_left = factor
-        while is_token?("*") || is_token?("/")
-        op = Ast::AstLeaf.new(@lexer.read)
-        tree_right = factor
-        tree_left = Ast::BinaryExpr.new([tree_left, op, tree_right])
+    def next_operator
+      token = @lexer.peek(0)
+      if token.is_identifier?
+        @operators[token.get_text]
+      else
+        nil
       end
-      tree_left
     end
 
-    def factor
+    def right_is_expr(prec, next_prec)
+      if next_prec.left_assoc
+        prec < next_prec.value
+      else
+        prec <= next_prec.value
+      end
+    end
+
+
+    def primary
       if is_token?("(")
         token("(")
-        e = expression
+        e = expr
         token(")")
         return e
       else
-        t = @lexer.read
-        if t.is_number?
-          n = Ast::NumberLiteral.new(t)
-          return n
+        token = @lexer.read
+        p token
+        if token.is_number?
+          return Ast::NumberLiteral.new(token)
+        elsif token.is_identifier?
+          return Ast::IdentifierLiteral.new(token)
+        elsif token.is_string?
+          return Ast::StringLiteral.new(token)
         else
-          raise "Parser Exception"
+          raise "Parse Exception in primary"
         end
       end
     end
 
+    def factor
+      if is_token?("-")
+        Ast::AstList.new([Ast::AstLeaf.new(@lexer.read), primary])
+      else
+        primary
+      end
+    end
+
+    def expr
+      right = factor
+      while (n = next_operator) != nil
+        right = do_shift(right, n.value)
+      end
+      right
+
+      #left = factor
+      #while is_token?("+") || is_token?("-") || is_token?("=") || is_token?("==")
+      #  left = Ast::BinaryExpr.new([left, Ast::AstLeaf.new(@lexer.read), factor])
+      #end
+      #left
+    end
+
+    def block
+      list = []
+      token("{")
+      list << statement unless is_token?(";") || is_token?("\n")
+      while ! is_token?("}")
+        @lexer.read
+        list << statement unless is_token?(";") || is_token?("\n") || is_token?("}")
+      end
+      token("}")
+      Ast::AstList.new(list)
+    end
+
+    def simple
+      expr
+    end
+
+    def statement
+      list = []
+      if is_token?("if")
+        list << Ast::AstLeaf.new(@lexer.read)
+        list << expr
+        list << block
+        if is_token?("else")
+          list << Ast::AstLeaf.new(@lexer.read)
+          list << block
+        end
+      elsif is_token?("while")
+        list << Ast::AstLeaf.new(@lexer.read)
+        list << expr
+        list << block
+      else
+        return simple
+      end
+      Ast::AstList.new(list)
+    end
+
+    def program
+      list = []
+      list << statement unless is_token?("\n") || is_token?(";")
+      if is_token?("\n") || is_token?(";")
+        @lexer.read
+      else
+        raise "Parse Exception in program"
+      end
+      Ast::AstList.new(list)
+    end
+
     def token(name)
       token = @lexer.read
-      unless token.is_indentifier? && name == token.get_text
-        raise "Parser Exception"
+      unless token.is_identifier? && name == token.get_text
+        raise "Parse Exception"
       end
     end
 
