1ac30b6c72245c766f31d192ca6d955c81c75bf1
Shohei Sasamoto <saxsir@net73-dhcp127.sfc.keio.ac.jp>
2012-11-08 15:37:09 +0900

    とりあえずpullのために

--- lib/stone.rb
+++ lib/stone.rb
@@ -6,6 +6,10 @@ require 'stone/ast/ast_tree'
 require 'stone/ast/ast_leaf'
 require 'stone/ast/ast_list'
 require 'stone/ast/number_literal'
+require 'stone/ast/null_stmnt'
 require 'stone/ast/binary_expr'
+
+require 'stone/basic_env'
+require 'stone/basic_evaluator'
 
 require 'stone/expr_parser'
