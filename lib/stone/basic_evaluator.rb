3d69a51a4c67745ad842481235daf489299b3944
Shohei Sasamoto <saxsir@Shoheis-MacBook-Air-3.local>
2012-11-15 00:29:19 +0900

    なんかwhile文動いたけど、空行があると動かない

--- lib/stone/basic_evaluator.rb
+++ lib/stone/basic_evaluator.rb
@@ -31,9 +31,26 @@ module Stone
       end
     end
     
-    class Name
+    class IdentifierLiteral
       def eval(env)
         value = env.get(self.name)
+
+        if value == nil
+          # 例外処理
+        else
+          value
+        end
+      end
+    end
+
+    class NegativeExpr
+      def eval(env)
+        value = operand().eval(env)
+        if value.kind_of?(Integer)
+          value * (-1)
+        else
+          # 例外処理
+        end
       end
     end
     
@@ -41,9 +58,9 @@ module Stone
       def eval(env)
         op = self.operator
         # javaのequalsはRubyの==と同じ. 値が等しいかどうかを調べる.
-        if "=" == op
+        if op == "="
           right = self.right.eval(env)
-          return comput_number(env, right)
+          return compute_assign(env, right)
         else
           left = self.left.eval(env)
           right = self.right.eval(env)
@@ -51,23 +68,23 @@ module Stone
         end
       end
 
+      def compute_assign(env, rvalue)
+        l = self.left()
+        # 教科書ではNameクラスだけどIdentifierLiteralクラスにしてる
+        # ast/number_literal.rb
+        if l.kind_of?(IdentifierLiteral)
+          env.put(l.name, rvalue)
+          rvalue
+        else
+          # 例外処理
+        end
+      end
+
       def compute_op(left, op, right)
         if (left.kind_of?(Integer) and right.kind_of?(Integer))
           return compute_number(left.to_i, op, right.to_i)
         else
-          if op == "+"
-            return left.to_s + right.to_s
-          elsif op == "*"
-            return left.to_s * right.to_s
-          elsif op == "=="
-            if left == nil
-              if right == nil then TRUE else FALSE end
-            else
-              if left == right then TRUE else FALSE end
-            end
-          else
-            # 例外処理
-          end
+          # 右辺か左辺が変数だった時？
         end
       end
       
@@ -83,16 +100,61 @@ module Stone
           when "/" then return a / b
           when "%" then return a % b
           when "=="
-            if a == b then return TRUE else return FALSE end
+            # if a == b then return TRUE else return FALSE end
+            a == b ? TRUE : FALSE 
           when ">"
-            if a > b then return TRUE else return FALSE end
+            # if a > b then return TRUE else return FALSE end
+            a > b ? TRUE : FALSE
           when "<"
-            if a < b then return TRUE else return FALSE end
+            # if a < b then return TRUE else return FALSE end
+            a < b ? TRUE : FALSE
           else
-            # caseのelseはswitchのdefaultと一緒
             # 例外処理
         end
       end
     end
+    
+    class BlockStmnt
+      def eval(env)
+        result = 0
+        self.children.each do |tree|
+          result = tree.eval(env)
+        end
+        result
+      end
+    end
+    
+    class IfStmnt
+      def eval(env)
+        # condition()ってなんだ
+        condition = condition().eval(env)
+        if ((condition.kind_of?(Integer)) and (condition.get_value != FALSE))
+          self.then_block().eval(env)
+        else
+          block = self.else_block()
+          if block == nil
+            0
+          else
+            block.eval(env)
+          end
+        end
+      end
+    end
+    
+    class WhileStmnt
+      def eval(env)
+        result = 0
+        loop do
+          condition = self.condition().eval(env)
+
+          if condition == FALSE
+            return result
+          else
+            result = self.body().eval(env)
+          end
+        end
+      end
+    end
+ 
   end
 end
